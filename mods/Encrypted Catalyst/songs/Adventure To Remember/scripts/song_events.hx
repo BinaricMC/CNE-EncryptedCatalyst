76cc94a32b27d008e159777bcbc286ea6ce2dc32
RequiemZeroGH <48424927+RequiemZeroGH@users.noreply.github.com>
2025-05-11 16:03:43 -0700

    Minor fixes and added new cam features

--- mods/Encrypted Catalyst/songs/Adventure To Remember/scripts/song_events.hx
+++ mods/Encrypted Catalyst/songs/Adventure To Remember/scripts/song_events.hx
@@ -1,11 +1,19 @@
-function postCreate() {
-    snapCam();
-}
-
 function onStartCountdown(event) {
     event.cancel(true);
 
     startSong();
     startedCountdown = true;
+}
+
+function stepHit(curStep:Int) {
+    switch(curStep) {
+        case 2656:
+            boyfriend.visible = false;
+            gf.visible = true;
+
+        case 5376:
+          boyfriend.visible = true;  
+
+    }
 }
 
---
+++ mods/Encrypted Catalyst/songs/camAngle.hx
@@ -0,0 +1,42 @@
+var cammove = 15;
+var angleMoveSpeed = 0.06;
+var angleVar = 0.45; 
+var cameraSpeed = 1;
+
+function postUpdate() {
+    switch(strumLines.members[curCameraTarget].characters[0].getAnimName()) {
+        case "singLEFT": 
+            camFollow.x -= cammove;
+            camGame.angle = (lerp(camGame.angle, -angleVar, angleMoveSpeed));
+        case "singDOWN": 
+            camFollow.y += cammove;
+            camGame.angle = (lerp(camGame.angle, 0, angleMoveSpeed));
+        case "singUP": 
+            camFollow.y -= cammove;
+            camGame.angle = (lerp(camGame.angle, 0, angleMoveSpeed));
+        case "singRIGHT": 
+            camFollow.x += cammove;
+            camGame.angle = (lerp(camGame.angle, angleVar, angleMoveSpeed));
+        case "idle", "hey":
+            camGame.angle = (lerp(camGame.angle, 0, angleMoveSpeed));
+    }
+
+    if (curCameraTarget == 0) {
+        defaultCamZoom = 0.6;
+    }
+    else if (curCameraTarget == 1) {
+        defaultCamZoom = 0.75;
+    }
+    else if (curCameraTarget == 2) {
+        defaultCamZoom = 0.84;
+    }
+
+
+}
+
+function postCreate() {
+    FlxG.camera.followLerp *= 2;
+    gf.visible = false;
+
+}
+     
