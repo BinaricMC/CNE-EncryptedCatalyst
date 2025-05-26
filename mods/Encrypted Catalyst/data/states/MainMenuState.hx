0f185e92e7eb322c3bf7911717fbda1ab4966ac8
ADA_Funni <codingking263@gmail.com>
2025-05-26 22:51:24 +0100

    zero grammar fix

--- mods/Encrypted Catalyst/data/states/MainMenuState.hx
+++ mods/Encrypted Catalyst/data/states/MainMenuState.hx
@@ -2,12 +2,10 @@ function update(){
     if (FlxG.keys.justPressed.C) {
         FlxG.sound.music.fadeOut(1, 0, t -> {
             FlxG.switchState(new ModState("MEOW"));
-
         });
     }
 }
 
 function create() {
     FlxG.sound.playMusic(Paths.music("freakyMenu"), 0.7);
-    
 }
