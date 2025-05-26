304b690bfc481be3d12d57738dbf1439090f55b2
ADA_Funni <codingking263@gmail.com>
2025-05-26 21:18:47 +0100

    Data changes yayyyyy
    Adverts now work through a JSON rather than a text file; this means I can now add arrays inside the arrays of the main array.

--- mods/Encrypted Catalyst/data/states/MEOW.hx
+++ mods/Encrypted Catalyst/data/states/MEOW.hx
@@ -1,29 +1,53 @@
-var memes:Array<String> = CoolUtil.coolTextFile('assets/data/randomshitlol.txt');
-var ads:Array<String> = CoolUtil.coolTextFile('assets/data/modAds.txt');
+import flixel.FlxObject;
 
-var adsList:Array<String, String> = [];
-
+var adsList:Array<Dynamic> = [];
 var curAdSelected:Int = 0;
 
-var adSelectedTxt:FlxText = new FlxText();
+var adSelectedTxt:FlxText;
+var camFollow:FlxObject;
 
-function create(){
+var bg:FlxSprite;
+
+/*
+[
+    [No Time For Funkin', https://www.youtube.com/watch?v=AhBYB4NfDlg],
+    [Test 2 (PREMEDITATED JX), https://www.youtube.com/watch?v=KIsa7i5hJ58],
+    [Test (NO URL)]
+]
+*/
+
+function create() {
+    bg = new FlxSprite(0, 0);
+    bg.scrollFactor.set(0.9, 0.9);
+    bg.screenCenter();
+    add(bg);
+
+    var dark:FlxSprite = new FlxSprite(0, 0).makeGraphic(FlxG.width * 1.1, FlxG.height * 1.1, FlxColor.BLACK);
+    add(dark);
+    dark.screenCenter();
+    dark.alpha = 0.6;
+
+    var bg2 = new FlxSprite(0, 0).loadGraphic(Paths.image('menus/titlescreen/newgrounds_logo'));
+    bg2.scrollFactor.set(0.4, 0.4);
+    add(bg2);
+
+    adSelectedTxt = new FlxText(0, 0, FlxG.width, "Testing testing 123", 32);
     adSelectedTxt.size = 50;
     adSelectedTxt.alignment = 'center';
     adSelectedTxt.screenCenter();
     adSelectedTxt.scrollFactor.set(0, 0);
     add(adSelectedTxt);
 
-    for (i in 0...ads.length)
-    {
-        split = ads[i].split('/:/');
-
-        adsList.push([split[0], (split[1] != null ? split[1] : memes[FlxG.random.int(0, memes.length - 1)])]);
-    }
-
+    adsList = CoolUtil.parseJson('data/modAds.json');
     trace(adsList);
 
     changeAd(0);
+
+    camFollow = new FlxObject(0, 0, 1, 1);
+    add(camFollow);
+
+    FlxG.camera.follow(camFollow, 0.005);
+    FlxG.mouse.visible = true;
 }
 
 function changeAd(e:Int){
@@ -31,13 +55,30 @@ function changeAd(e:Int){
 
     adSelectedTxt.text = adsList[curAdSelected][0];
     adSelectedTxt.screenCenter(FlxAxes.X);
+
+    loadImg();
+}
+
+function loadImg() {
+    var img = adsList[curAdSelected][2];
+    bg.loadGraphic(Paths.image('menus/advert/banner/' + img));
+
+    bg.setGraphicSize(FlxG.width * 0.6, FlxG.height * 0.6);
+    bg.updateHitbox();
+
+    bg.screenCenter();
 }
 
 function selectAd(){
-    CoolUtil.openURL(adsList[curAdSelected][1]);
+    var url = adsList[curAdSelected][1];
+
+    if (url != null) CoolUtil.openURL(url);
+    else trace('No URL in json, do nothing');
 }
 
-function update(){
+function update(elapsed:Float){
+    camFollow.setPosition((FlxG.width - (FlxG.mouse.x / 90)) / 2, (FlxG.height - (FlxG.mouse.y / 90)) / 2);
+
     if (controls.LEFT_P) changeAd(-1);
     if (controls.RIGHT_P) changeAd(1);
     if (controls.ACCEPT) selectAd();
