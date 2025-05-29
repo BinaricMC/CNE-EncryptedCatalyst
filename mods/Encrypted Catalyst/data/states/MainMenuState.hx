35ee54d808850887c7d3b0f96af0bde6f15a9137
sillyborja <sillyburgerprivalt@gmail.com>
2025-05-29 12:10:24 -0300

    small menu stuff please improve ts

--- mods/Encrypted Catalyst/data/states/MainMenuState.hx
+++ mods/Encrypted Catalyst/data/states/MainMenuState.hx
@@ -1,3 +1,5 @@
+var txts:FlxGroup;
+
 function update(){
     if (FlxG.keys.justPressed.C) {
         FlxG.sound.music.fadeOut(1, 0, t -> {
@@ -12,4 +14,22 @@ function create() {
     // Only plays the menu music if nothing else is playing
     if (!FlxG.sound.music.playing) FlxG.sound.playMusic(Paths.music("freakyMenu"), 0.7);
     camera.flash(FlxColor.BLACK, 0.95);
+
+    txts = new FlxGroup();
+    add(txts);
+
+    var toAdd = ["STORY MODE", "FREEPLAY", "CREDITS & EXTRAS", "ADVERTISEMENTES", "OPTIONS"];
+    for (i in 0...toAdd.length) createText(toAdd[i], [50, (130 + (i * 100))]);
+}
+
+function createText(string:String, pos:Array<Float>){
+    text = new FlxText();
+    text.text = string;
+    text.size = 40;
+    text.scrollFactor.set(0, 0);
+    text.x = pos[0];
+    text.y = pos[1];
+
+    add(text);
+    txts.add(text);
 }
