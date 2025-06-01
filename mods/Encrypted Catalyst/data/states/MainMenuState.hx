bdc6865c5c3d303b187e093b4a9bf51362898956
mc <nguyenkyle23@gmail.com>
2025-05-31 17:15:48 -0700

    stinky code

--- mods/Encrypted Catalyst/data/states/MainMenuState.hx
+++ mods/Encrypted Catalyst/data/states/MainMenuState.hx
@@ -1,29 +1,56 @@
 import funkin.menus.ModSwitchMenu;
 import flixel.effects.FlxFlicker;
+import funkin.editors.EditorPicker;
 
+import openfl.ui.Mouse;
+
+// text functions
 var txts:FlxGroup;
+var lowerText:FlxText;
+var typeSpeed:Float = 0.05;
+var tabText:FlxText;
 
+// menu functions
 var curSelected:Int = 0;
-
 var canSelect:Bool = true;
+var menus = ["StoryMode", "FreeplayState", "Credits", "MEOW", "Options"];
 
-var menus = [
-    "StoryMode",
-    "FreeplayState",
-    "Credits",
-    "MEOW",
-    "Options"
-];
-function update(){
-    if (canSelect){
-        if (controls.SWITCHMOD && FlxG.state.subState == null) openSubState(new ModSwitchMenu());
-        if (controls.DOWN_P) changeSelect(1);
-        if (controls.UP_P) changeSelect(-1);
-        if (controls.ACCEPT) select();
-    }
-}
 
 function create() {
+    //background shit
+    aura = new FunkinSprite(300, -500).loadGraphic(Paths.image('menus/mainmenu/aura'));
+    aura.scale.set(0.8, 0.8);
+    add(aura);
+
+    background = new FunkinSprite(-720, -400).loadGraphic(Paths.image('menus/mainmenu/background'));
+    background.scale.set(0.5, 0.5);
+    add(background);
+
+    outlines = new FunkinSprite(-710, -407).loadGraphic(Paths.image('menus/mainmenu/outlines'));
+    outlines.scale.set(0.5, 0.5);
+    add(outlines);
+
+    //text
+    new FlxTimer().start(2, function(tmr:FlxTimer)
+        {
+            lowerText = new FlxTypeText(5, FlxG.height - 10, 0, '"AN FNF MOD BASED ON THE TALES OF DODGE GREENLY IN THE ARTISTIC STYLE AND VISION OF O0OPS."');
+            lowerText.y -= lowerText.height;
+            lowerText.size = 20;
+            lowerText.scrollFactor.set();
+            lowerText.font = Paths.font("black-ground.ttf");
+            lowerText.start(typeSpeed);
+            lowerText.antialiasing = Options.antialiasing;
+            lowerText.color = FlxColor.GRAY;
+	        add(lowerText);
+        });
+    
+    tabText = new FlxText(1020, FlxG.height - 9, 0, 'Press TAB to Open Mods Menu');
+    tabText.y -= tabText.height;
+    tabText.font = Paths.font("black-ground.ttf");
+    tabText.size = 20;
+    tabText.scrollFactor.set();
+    add(tabText);
+
     // Only plays the menu music if nothing else is playing
     if (!FlxG.sound.music.playing) FlxG.sound.playMusic(Paths.music("freakyMenu"), 0.7);
     camera.flash(FlxColor.BLACK, 0.95);
@@ -31,19 +58,47 @@ function create() {
     txts = new FlxGroup();
     add(txts);
 
-    var toAdd = ["STORY MODE", "FREEPLAY", "CREDITS & EXTRAS", "ADVERTISEMENTES", "OPTIONS"];
+    var toAdd = ["STORY MODE", "FREEPLAY", "CREDITS & EXTRAS", "ADVERTISEMENTS", "OPTIONS"];
     for (i in 0...toAdd.length) createText(toAdd[i], [50, (130 + (i * 100))]);
 
     changeSelect(0);
 }
 
+function postCreate()
+    FlxG.mouse.visible = true;
+
+function update(){
+    if (canSelect) {
+        if (controls.SWITCHMOD && FlxG.state.subState == null) 
+            openSubState(new ModSwitchMenu());
+
+        if (controls.DOWN_P) 
+            changeSelect(1);
+
+        if (controls.UP_P) 
+            changeSelect(-1);
+
+        if (controls.ACCEPT) 
+            select();
+    }
+
+    if (FlxG.keys.justPressed.SEVEN) {
+        openSubState(new EditorPicker());
+
+        persistentUpdate = false;
+        persistentDraw = true;
+    }
+}
+
+
 function createText(string:String, pos:Array<Float>){
     text = new FlxText();
     text.text = string;
-    text.size = 40;
+    text.size = 52;
     text.scrollFactor.set(0, 0);
     text.x = pos[0];
     text.y = pos[1];
+    text.font = Paths.font("black-ground.ttf");
     text.antialiasing = Options.antialiasing;
 
     add(text);
@@ -62,8 +117,21 @@ function changeSelect(e:Int){
 function select(){
     CoolUtil.playMenuSFX(1);
     FlxFlicker.flicker(txts.members[curSelected], 1, 0.04, true, false);
+    
+    if (menus[curSelected] == "StoryMode")
+        FlxG.switchState(new StoryMenuState());
 
-    if (menus[curSelected] == 'MEOW') FlxG.sound.music.fadeOut(0.95, 0, t -> FlxG.sound.playMusic(Paths.music("creditstheme"), 0.7));
+    if (menus[curSelected] == "FreeplayState")
+        FlxG.switchState(new FreeplayState());
+
+    if (menus[curSelected] == "Credits")
+        FlxG.switchState(new CreditsMain());
+
+    if (menus[curSelected] == 'MEOW') 
+        FlxG.sound.music.fadeOut(0.95, 0, t -> FlxG.sound.playMusic(Paths.music("creditstheme"), 0.7));
+
+    if (menus[curSelected] == "Options")
+        FlxG.switchState(new OptionsMenu());
 
     new FlxTimer().start(1, function() FlxG.switchState(new ModState(menus[curSelected])));
 }
