caa7bd1361b7ec6894c7519bb740eb545788cb3a
sillyborja <sillyburgerprivalt@gmail.com>
2025-07-22 16:50:38 -0300

    ik mod won't be updated anymore but
    god this code so ass i had to update it because it wouldn't stop bothering me

--- mods/Encrypted Catalyst/data/transitions/glitch.hx
+++ mods/Encrypted Catalyst/data/transitions/glitch.hx
@@ -4,21 +4,23 @@ function create(event){
     event.cancel();
 
     for (i in FlxG.cameras.list) applyGlitch(i);
-    FlxTween.num((event.transOut ? 0 : 2), (event.transOut ? 2 : 0), 0.5, {ease: (event.transOut ? FlxEase.quadIn : FlxEase.quadOut), onUpdate: function(e) {
-        shader.prob = e.value * 2;
-        shader.time = e.value;
-        shader.intensityChromatic = e.value / 2;
-    }, onComplete: function() finishTrans(), type: FlxTween.ONESHOT});
+    FlxTween.num(
+        (event.transOut ? 0 : 2),
+        (event.transOut ? 2 : 0),
+        0.5,
+        {
+            ease: (event.transOut ? FlxEase.quadIn : FlxEase.quadOut),
+            onComplete: function() finish()
+        },
+        function (num:Float) {
+            shader.prob = num * 2;
+            shader.time = num;
+            shader.intensityChromatic = num / 2;
+        }
+        );
 }
 
 function applyGlitch(camera:FlxCamera)
 {
     camera.addShader(shader);
-}
-
-function finishTrans(){
-    shader.time = 0;
-    shader.prob = 0;
-    shader.intensityChromatic = 0;
-    finish();
 }
