50825430cf756f6098c478ad80be6a10ded1e57d
ADA_Funni <codingking263@gmail.com>
2025-08-23 19:27:02 +0100

    Replace CNE build with mod folder.

--- addons/GameJolt Addon/data/states/MainMenuState.hx
+++
@@ -1,9 +0,0 @@
-import funkin.editors.UIDebugState;
-import funkin.editors.ui.UIState;
-
-function onSelectItem(event) {
-	switch(event.name) {
-		case 'login':
-			FlxG.switchState(new UIState(true, "LoginState"));
-	}
-}
